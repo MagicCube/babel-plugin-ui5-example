1ad2ef31ae31b5c4dbacb7004140645cb04606fd
Henry Li <henry.li03@sap.com>
2016-04-14 13:24:18 +0800

    Add 'dev' task and sourcemap support.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const rename = require("gulp-rename");
 const runSequence = require("run-sequence");
 const uglify = require("gulp-uglify");
 const ui5Lib = require("gulp-ui5-lib");
+const watch = require("gulp-watch");
 
 const SRC_ROOT = "./client-src";
 const ASSETS_ROOT = "./public/assets";
@@ -15,6 +16,7 @@ const MODULES = [ "example" ];
 MODULES.forEach(module => {
     // See generateBuildModuleTask(modul) bellow
     generateBuildModuleTask(module);
+    generateBuildDevModuleTask(module);
 });
 
 gulp.task("default", [ "build" ]);
@@ -38,16 +40,30 @@ gulp.task("build", [ "clean" ], cb => {
     runSequence.apply(this, params);
 });
 
+gulp.task("dev", [ "clean" ], cb => {
+    // Let's build modules one by one,
+    const params = MODULES.map(module => `build-dev:${module}`);
+
+    // Don't forget our callback.
+    params.push(cb);
+
+    // Execute the workflow.
+    runSequence.apply(this, params);
+});
 
 
-// This will bring modulization concept to our UI5 project.
-// Think your application or reusable library as a independent module.
+
+
+
+
 function generateBuildModuleTask(module)
 {
     gulp.task(`build:${module}`, cb => {
         runSequence(
-            `build-less:${module}`,
-            `build-js:${module}`,
+            [
+                `build-less:${module}`,
+                `build-js:${module}`,
+            ],
             `build-library:${module}`,
             cb
         );
@@ -70,5 +86,33 @@ function generateBuildModuleTask(module)
                    .pipe(uglify())
                    .pipe(ui5Lib(`${module}`))
                    .pipe(gulp.dest(`${ASSETS_ROOT}/${module}`));
+    });
+}
+
+function generateBuildDevModuleTask(module)
+{
+    gulp.task(`build-dev:${module}`, cb => {
+        runSequence(
+            [
+                `build-dev-less:${module}`,
+                `build-dev-js:${module}`,
+            ],
+            cb
+        );
+    });
+
+    gulp.task(`build-dev-js:${module}`, () => {
+        const sourceFiles = `${SRC_ROOT}/${module}/**/*.js`;
+        return gulp.src(sourceFiles)
+                   .pipe(watch(sourceFiles))
+                   .pipe(babel({ sourceMaps: "inline" }))
+                   .pipe(gulp.dest(`${ASSETS_ROOT}/${module}`));
+    });
+
+    gulp.task(`build-dev-less:${module}`, () => {
+        return gulp.src(`${SRC_ROOT}/${module}/themes/base/library.less`)
+            .pipe(watch(`${SRC_ROOT}/${module}/themes/base/**/*.less`))
+            .pipe(less())
+            .pipe(gulp.dest(`${ASSETS_ROOT}/${module}/themes/base`));
     });
 }
